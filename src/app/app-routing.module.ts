599e5c684d562a8376fd126b1e9764365d789084
pritamkamble <7276279026.pk@gmail.com>
2019-08-30 17:03:07 +0530

    minimal working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DraggableComponent } from './draggable/draggable.component';
+import { MinimalComponent } from './minimal/minimal.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'draggable', pathMatch: 'full' },
+  { path: '', redirectTo: 'minimal', pathMatch: 'full' },
   {
     path: 'draggable',
     component: DraggableComponent,
+  },
+  {
+    path: 'minimal',
+    component: MinimalComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DraggableComponent } from './draggable/draggable.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MinimalComponent } from './minimal/minimal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DraggableComponent
+    DraggableComponent,
+    MinimalComponent
   ],
   imports: [
     BrowserModule,
